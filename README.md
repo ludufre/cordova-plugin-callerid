aedb64e7d91198cc2f203878da78b055ffb77ee2
Luan Freitas <ludufre@gmail.com>
2020-07-15 10:27:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Add the plugin to your Cordova project:
+
+`cordova plugin add cordova-plugin-callerid`
---
+++ src/ios/CordovaCallerID.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDVPlugin.h>
+
+@interface CallerID : CDVPlugin
+
+- (void)echo:(CDVInvokedUrlCommand*)command;
+
+@end
